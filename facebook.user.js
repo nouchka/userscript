2adddce0f5d5a72fbdf534cd6ce354a4f05d7e09
nouchka <github@katagena.com>
2016-08-09 18:23:57 +0200

    override all

--- facebook.user.js
+++ facebook.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Facebook styling
 // @namespace    http://www.katagena.com/
-// @version      0.2
+// @version      0.4
 // @description  no right column, fixed japan background
 // @author       Jean-Avit Promis
 // @match        https://www.facebook.com/
@@ -11,5 +11,5 @@
 (function() {
     'use strict';
 
-    GM_addStyle("body{background-image: url('//img0.mxstatic.com/wallpapers/b6b285d9b8411ab3d12e5bd0b139fdd9_large.jpeg');background-attachment: fixed;}#contentCol{width: 502px;}#rightCol{display:none;}");
+    GM_addStyle("body{background-image: url('//img0.mxstatic.com/wallpapers/b6b285d9b8411ab3d12e5bd0b139fdd9_large.jpeg') !important;background-attachment: fixed !important;}#contentCol{width: 502px;}#rightCol{display:none;}");
 })();
