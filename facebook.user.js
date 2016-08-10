70de28dcf7dab03d009269261c9d6512c51dd1e0
Jean-Avit Promis <nouchka@users.noreply.github.com>
2016-08-10 11:47:13 +0200

    hide counter

--- facebook.user.js
+++ facebook.user.js
@@ -11,5 +11,5 @@
 (function() {
     'use strict';
 
-    GM_addStyle("body{background-image: url('//img0.mxstatic.com/wallpapers/b6b285d9b8411ab3d12e5bd0b139fdd9_large.jpeg') !important;background-attachment: fixed !important;}#contentCol{width: 502px;}#rightCol{display:none;}.countValue{display: none;}");
+    GM_addStyle("body{background-image: url('//img0.mxstatic.com/wallpapers/b6b285d9b8411ab3d12e5bd0b139fdd9_large.jpeg') !important;background-attachment: fixed !important;}#contentCol{width: 502px;}#rightCol{display:none;}.countValue{display: none !important;}");
 })();
