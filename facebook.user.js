7749af61ef210205a35e40d502493f4834cae172
nouchka <github@katagena.com>
2016-12-23 16:26:58 +0100

    new wallpaper

--- facebook.user.js
+++ facebook.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Facebook styling
 // @namespace    http://www.katagena.com/
-// @version      0.8
+// @version      0.9
 // @description  no right column, fixed japan background
 // @author       Jean-Avit Promis
 // @match        https://www.facebook.com/*
@@ -11,5 +11,5 @@
 (function() {
     'use strict';
 
-    GM_addStyle("body{background-image: url('//wallpaper-gallery.net/images/japan-wallpaper/japan-wallpaper-10.jpg') !important;background-attachment: fixed !important;}#contentCol{width: 502px;}#rightCol{display:none;}.countValue{display: none !important;}");
+    GM_addStyle("body{background-image: url('//static.katagena.com/kobe/wallpaper.jpg') !important;background-attachment: fixed !important;}#contentCol{width: 502px;}#rightCol{display:none;}.countValue{display: none !important;}");
 })();
