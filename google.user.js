b2307178dddc9ae9311cd68984a802ba103f95f0
nouchka <github@katagena.com>
2018-05-05 17:15:19 +0000

    clean include + space

--- google.user.js
+++ google.user.js
@@ -1,26 +1,25 @@
 // ==UserScript==
 // @name           No Google Search Redirect (Use Jquery)
-// @version        1.9
-// @author         imt 
+// @version        1.10
+// @author         imt
 // @run-at         document-end
-// @include        http://www.google.*/*
-// @include        https://www.google.*/* 
+// @include        https://www.google.com/search*
 // @require        http://code.jquery.com/jquery-2.1.0.min.js
 // ==/UserScript== 
 
 
 
-function formatSearch(){ 
-    $("#ires a").each(function(){ 
+function formatSearch(){
+    $("#ires a").each(function(){
         $(this).removeAttr("onmousedown");
         var real =$(this).attr("data-href");
         if(real)
-            $(this).attr("href",$(this).attr("data-href")); 
-    }); 
+            $(this).attr("href",$(this).attr("data-href"));
+    });
 }
-$('#main').on("DOMNodeInserted", function(e) {  
+$('#main').on("DOMNodeInserted", function(e) {
     formatSearch();
-}); 
+});
 $(function(){
     formatSearch();
 });
