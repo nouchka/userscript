b882a3b85d3bb09667f303b8a510627b881c598f
nouchka <github@katagena.com>
2018-05-17 09:16:08 +0000

    stagram.com RSS for instagram

---
+++ instagram.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           Add RSS from stagram.com
+// @version        1.0
+// @author         nouchka
+// @run-at         document-end
+// @include        https://www.instagram.com/*
+// @require        http://code.jquery.com/jquery-2.1.0.min.js
+// @grant        none
+// ==/UserScript==
+
+$(function(){
+    //don't care if undefined
+    $("head").append('<link rel="alternate" type="application/rss+xml" title="Instagram RSS" href="https://web.stagram.com/rss/n/'+$($("h1")[0]).attr('title')+'/"/>');
+});
